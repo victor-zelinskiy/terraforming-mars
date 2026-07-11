8a6f001f929f4311537e99e966c30c69033f8a99
rusliksu <25394128+rusliksu@users.noreply.github.com>
2026-07-11 23:45:36 +0300

    Skip Floating Trade Hub target prompt for single card (#8296)
    * Skip Floating Trade Hub target prompt for single card
    
    * Address Floating Trade Hub review
    
    * Clarify Floating Trade Hub tests
    
    * Flatten Floating Trade Hub tests
    
    * Update test descriptions for FloatingTradeHub.spec.ts
    
    ---------
    
    Co-authored-by: Robert Konigsberg <kberg@users.noreply.github.com>

--- src/server/cards/prelude2/FloatingTradeHub.ts
+++ src/server/cards/prelude2/FloatingTradeHub.ts
@@ -2,11 +2,12 @@ import {Tag} from '../../../common/cards/Tag';
 import {CardName} from '../../../common/cards/CardName';
 import {CardRenderer} from '../render/CardRenderer';
 import {CardResource} from '../../../common/CardResource';
-import {IActionCard} from '../ICard';
+import {IActionCard, ICard} from '../ICard';
 import {PreludeCard} from '../prelude/PreludeCard';
 import {IPlayer} from '../../IPlayer';
 import {OrOptions} from '../../inputs/OrOptions';
 import {SelectCard} from '../../inputs/SelectCard';
+import {SelectOption} from '../../inputs/SelectOption';
 import {AndOptions} from '../../inputs/AndOptions';
 import {SelectAmount} from '../../inputs/SelectAmount';
 import {SelectResource} from '../../inputs/SelectResource';
@@ -35,10 +36,12 @@ export class FloatingTradeHub extends PreludeCard implements IActionCard {
   }
 
   public action(player: IPlayer) {
-    const add2Floaters = new SelectCard('Select card to gain 2 floaters', undefined, player.getResourceCards(CardResource.FLOATER)).andThen(([card]) => {
-      player.addResourceTo(card, {qty: 2, log: true});
-      return undefined;
-    });
+    const floaterCards = player.getResourceCards(CardResource.FLOATER);
+    const floatingTradeHub = floaterCards.find((card) => card.name === this.name);
+    if (this.resourceCount === 0 && floaterCards.length === 1 && floatingTradeHub !== undefined) {
+      return this.add2Floaters(player, floatingTradeHub);
+    }
+    const add2Floaters = this.add2FloatersOption(player, floaterCards, floatingTradeHub);
     const selectResource = new SelectResource('Select resource to gain');
     const selectAmount = new SelectAmount('Select amount of floaters to remove', undefined, 1, this.resourceCount, true);
     const removeFloaters = new AndOptions(selectAmount, selectResource)
@@ -53,5 +56,21 @@ export class FloatingTradeHub extends PreludeCard implements IActionCard {
       return add2Floaters;
     }
     return new OrOptions(add2Floaters, removeFloaters);
+  }
+
+  private add2FloatersOption(player: IPlayer, floaterCards: Array<ICard>, floatingTradeHub: ICard | undefined) {
+    if (floaterCards.length === 1 && floatingTradeHub !== undefined) {
+      return new SelectOption('Add 2 floaters to Floating Trade Hub', 'Add floaters').andThen(() => {
+        return this.add2Floaters(player, floatingTradeHub);
+      });
+    }
+    return new SelectCard('Select card to gain 2 floaters', undefined, floaterCards).andThen(([card]) => {
+      return this.add2Floaters(player, card);
+    });
+  }
+
+  private add2Floaters(player: IPlayer, card: ICard): undefined {
+    player.addResourceTo(card, {qty: 2, log: true});
+    return undefined;
   }
 }
